6cb16668f755443226cc67142c31095223b21c39
Grimm Mark <grimmmark@mail.de>
2023-01-26 13:34:25 +0100

    Changed names, table => Tisch

--- src/App.jsx
+++ src/App.jsx
@@ -59,16 +59,16 @@ function App() {
               <Route path="/Kitchen" id="relement" element={<Kitchen />} />
               <Route path="/Bill" id="relement" element={<Bill />} />
               <Route path="/Bon" id="relement" element={<Bon />} />
-              <Route path="/Table1" id="relement" element={<Table1 />} />
-              <Route path="/Table2" id="relement" element={<Table2 />} />
-              <Route path="/Table3" id="relement" element={<Table3 />} />
-              <Route path="/Table4" id="relement" element={<Table4 />} />
-              <Route path="/Table5" id="relement" element={<Table5 />} />
-              <Route path="/Table6" id="relement" element={<Table6 />} />
-              <Route path="/Table7" id="relement" element={<Table7 />} />
-              <Route path="/Table8" id="relement" element={<Table8 />} />
-              <Route path="/Table9" id="relement" element={<Table9 />} />
-              <Route path="/Table10" id="relement" element={<Table10 />} />
+              <Route path="/Tisch1" id="relement" element={<Table1 />} />
+              <Route path="/Tisch2" id="relement" element={<Table2 />} />
+              <Route path="/Tisch3" id="relement" element={<Table3 />} />
+              <Route path="/Tisch4" id="relement" element={<Table4 />} />
+              <Route path="/Tisch5" id="relement" element={<Table5 />} />
+              <Route path="/Tisch6" id="relement" element={<Table6 />} />
+              <Route path="/Tisch7" id="relement" element={<Table7 />} />
+              <Route path="/Tisch8" id="relement" element={<Table8 />} />
+              <Route path="/Tisch9" id="relement" element={<Table9 />} />
+              <Route path="/Tisch10" id="relement" element={<Table10 />} />
               <Route path="/products" id="relement" element={<products />} />
               <Route path="/Bediener1" id="relement" element={<Bediener1 />} />
               <Route path="/Bediener2" id="relement" element={<Bediener2 />} />
--- src/data/tables.jsx
+++ src/data/tables.jsx
@@ -6,52 +6,52 @@ import { useState } from 'react';
 export const tables = [
   {
     ID: 0,
-    name: 'Table1',
+    name: 'Tisch1',
     Bediener: 'Bediener1',
   },
   {
     ID: 1,
-    name: 'Table2',
+    name: 'Tisch2',
     Bediener: 'Bediener1',
   },
   {
     ID: 2,
-    name: 'Table3',
+    name: 'Tisch3',
     Bediener: 'Bediener1',
   },
   {
     ID: 3,
-    name: 'Table4',
+    name: 'Tisch4',
     Bediener: 'Bediener1',
   },
   {
     ID: 4,
-    name: 'Table5',
+    name: 'Tisch5',
     Bediener: 'Bediener1',
   },
   {
     ID: 5,
-    name: 'Table6',
+    name: 'Tisch6',
     Bediener: 'Bediener1',
   },
   {
     ID: 6,
-    name: 'Table7',
+    name: 'Tisch7',
     Bediener: 'Bediener1',
   },
   {
     ID: 7,
-    name: 'Table8',
+    name: 'Tisch8',
     Bediener: 'Bediener1',
   },
   {
     ID: 8,
-    name: 'Table9',
+    name: 'Tisch9',
     Bediener: 'Bediener1',
   },
   {
     ID: 9,
-    name: 'Table10',
+    name: 'Tisch10',
     Bediener: 'Bediener1',
   },
 
